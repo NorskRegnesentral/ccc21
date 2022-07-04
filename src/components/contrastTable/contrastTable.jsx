f31124afa8ec381b84c5ce616c6b23df601d14c1
Kamilla Mortensen <kamilla@nr.no>
2022-07-04 11:23:11 +0200

    tabell viser farger i små bokser

--- src/components/contrastTable/contrastTable.jsx
+++ src/components/contrastTable/contrastTable.jsx
@@ -40,7 +40,7 @@ const ContrastTable = ({colorList}) => {
                 {tableList.map((color, rowIndex) => (
                 <tr key={"row"+rowIndex}>
                     {tableList.map((color, colIndex) => (
-                        <td key={rowIndex+colIndex}  style={{backgroundColor: getCellColor(rowIndex,colIndex)}}>{getCellValue(rowIndex, colIndex)}</td>
+                        <td key={rowIndex+colIndex}><div className="colorBox" style={{backgroundColor: getCellColor(rowIndex,colIndex)}}/><div>{getCellValue(rowIndex, colIndex)}</div></td> //sett backgroundcolor i ytterste div for full dekning
                     ))}
                 </tr>
                 ))}
