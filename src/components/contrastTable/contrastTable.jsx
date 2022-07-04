8ee7eec3e319143ab7bf6e2a9960f5e05e0246b8
Kamilla Mortensen <kamilla@nr.no>
2022-07-04 14:49:17 +0200

    farger i tabell

--- src/components/contrastTable/contrastTable.jsx
+++ src/components/contrastTable/contrastTable.jsx
@@ -4,6 +4,7 @@ import {checkColors} from "../../color-checker";
 
 const ContrastTable = ({colorList}) => {
   const [tableList, setTableList] = useState([]);
+  const [contrastColors] = useState({none:"#F2B8B8",AA:"#F2E9B8",AAA:"#B7F1B7"});
 
   //tabell-listen skal alltid være den samme som fargelisten, 
   //men med ett tomt felt først
@@ -11,10 +12,20 @@ const ContrastTable = ({colorList}) => {
     setTableList([""].concat(colorList));
   },[colorList]);
 
-  const getCellColor = (rowIndex, columnIndex) => {
+  const getCellColorFromHex = (rowIndex, columnIndex) => {
     if(rowIndex === 0) return tableList[columnIndex];
     else if (columnIndex === 0) return tableList[rowIndex];
     return; 
+  }
+
+  const getCellColorFromContrast = (rowIndex, columnIndex) => {
+    if(rowIndex === 0 || columnIndex === 0 || rowIndex === columnIndex) return "";
+    const color1 =  tableList[columnIndex];
+    const color2 = tableList[rowIndex];
+    const contrast = checkColors(color1, color2).contrast; 
+    if(contrast < 4.5) return contrastColors.none;
+    if(4.5 <= contrast && contrast < 7.0) return contrastColors.AA;
+    return contrastColors.AAA; 
   }
 
   const getCellValue = (rowIndex, columnIndex) => {
@@ -28,7 +39,6 @@ const ContrastTable = ({colorList}) => {
   const getContrast = (rowIndex, columnIndex) => {
     const color1 =  tableList[columnIndex];
     const color2 = tableList[rowIndex];
-
     const contrast = checkColors(color1, color2); 
     return contrast.contrast; //contrast har mange verdier. Console.log for å se alle muligheter:)
   }
@@ -40,7 +50,7 @@ const ContrastTable = ({colorList}) => {
                 {tableList.map((color, rowIndex) => (
                 <tr key={"row"+rowIndex}>
                     {tableList.map((color, colIndex) => (
-                        <td key={rowIndex+colIndex} ><div style={{'backgroundColor': getCellColor(rowIndex,colIndex)}} className="colorBox"/><div>{getCellValue(rowIndex, colIndex)}</div></td> //sett backgroundcolor i ytterste div for full dekning
+                        <td key={rowIndex+colIndex} style={{'backgroundColor': getCellColorFromContrast(rowIndex,colIndex)}}><div style={{'backgroundColor': getCellColorFromHex(rowIndex,colIndex)}} className="colorBox"/><div>{getCellValue(rowIndex, colIndex)}</div></td> //sett backgroundcolor i ytterste div for full dekning
                     ))}
                 </tr>
                 ))}
