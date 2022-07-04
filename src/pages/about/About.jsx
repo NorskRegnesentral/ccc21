bbf2da20729eeea6b1464f971f89f9fde38e15c5
Kamilla Mortensen <kamilla@nr.no>
2022-07-04 16:18:50 +0200

    startet arbeid for kontrast-matrise

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,12 +1,10 @@
 import "./about.css";
 import React, { useEffect, useState } from "react";
-import Timeline from "../../components/timeline/timeline";
 
 function About() {
   return (
     <div className="About">
       <h1>Hva er WCAG?</h1>
-      <Timeline />
     </div>
   );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,34 +3,76 @@ import React, { useEffect, useState } from "react";
 import NavBar from "../../components/navBar/navBar";
 import ColorInput from "../../components/colorInput/colorInput";
 import ContrastTable from "../../components/contrastTable/contrastTable";
+import ContrastPreview  from "../../components/contrastPreview/contrastPreview";
+import {checkColors} from "../../color-checker"; 
 
 function Home() {
   const [colorList, setColorList] = useState(["#000000"]);
-  useEffect(() => {},[]);
+  const [tableList, setTableList] = useState([]);
+  const [contrastMatrix, setContrastMatrix] = useState();
+
+   //tabell-listen skal alltid være den samme som fargelisten, 
+  //men med ett tomt felt først
+  useEffect(() => {
+    setTableList([""].concat(colorList));
+    console.log("oppdater kontrast matrise")
+    const table2 = []
+    for(let row in tableList){
+      let rowObject = []
+      for(let column in tableList){
+        //rowObject.push((row+column))
+        rowObject.push(getCellValue(row,column))
+      }
+      table2.push(rowObject)
+    }
+    console.log("tabell2", table2)
+  },[colorList]);
+
+  const getCellValue = (rowIndex, columnIndex) => {
+    console.log("get cell value", rowIndex + "og" + columnIndex)
+    console.log("table list atm", tableList)
+    if(rowIndex === 0) return tableList[columnIndex];
+    else if (columnIndex === 0) return tableList[rowIndex];
+    if(rowIndex === columnIndex) return "" //returnerer tom fordi det er samme fargene
+    return "kommer";
+    //return getContrast(rowIndex,columnIndex); 
+  }
+
+  //bruker den importerte metoden fra color API 
+  const getContrast = (rowIndex, columnIndex) => {
+    const color1 =  tableList[columnIndex];
+    console.log("color 2", tableList[rowIndex])
+    const color2 = tableList[rowIndex];
+    console.log("finner fargekontrast", color1 + "og" + color2)
+    const contrast = checkColors(color1, color2); 
+    return contrast.contrast; //contrast har mange verdier. Console.log for å se alle muligheter:)
+  }
 
   /** Metoden oppdaterer en farge i listen når du endrer fargen i brukergrensesnittet */
-  var updateColorValue = (index, newValue) => {
+  // todo: flytt inn i myColors-component
+  const updateColorValue = (index, newValue) => {
     setColorList(colors => colors.map((value, i) => i === index ? newValue : value))
   }
 
-  var removeColorValue = (index) => {
+  // todo: flytt inn i myColors-component
+  const removeColorValue = (index) => {
     setColorList((colorList) => colorList.filter((_, i) => i !== index.index))
   }
+
+
 
   return (
     <div className="App">
       <NavBar></NavBar>
       <div className="content">
         <div className="contentGroup">
-          <h1>Farger</h1>
+          <h1>Dine farger</h1>          
           <div className="inputFields">
             {colorList.map((color, index) => (
               <ColorInput key={"color"+index} index={index} colorValue={color} updateColorValue={updateColorValue} removeColorValue={removeColorValue} ></ColorInput>
             ))}
           </div>
-          <button
-            onClick={() => setColorList((colorList) => [...colorList, "#000000"])}
-          >
+          <button onClick={() => setColorList((colorList) => [...colorList, "#000000"])}>
             legg til fargekode
           </button>
         </div>
@@ -40,6 +82,8 @@ function Home() {
         </div>
         <div className="contentGroup">
           <h1>Her kommer visning av komponenter i fargene</h1>
+          <ContrastPreview title="gode matcher"></ContrastPreview>
+          <ContrastPreview title="dårligere matcher"></ContrastPreview>
         </div>
       </div>
     
