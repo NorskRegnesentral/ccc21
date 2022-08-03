a2c7a141480bc0de92547bfa2f38bfdfa48f2635
Kamilla Mortensen <kamilla@nr.no>
2022-08-03 14:16:08 +0200

    graph fic

--- src/components/mockupGraph/mockupGraph.jsx
+++ src/components/mockupGraph/mockupGraph.jsx
@@ -2,12 +2,10 @@ import "./mockupGraph.css";
 import React, {useState, useEffect} from "react";
 
 const MockupGraph = ({ colorList }) => {
-    const [degreesPerColor, setDegreesPerColor]= useState(360/colorList.length);
     const [pieChartValues, setPieChartValues] = useState();
+    
     useEffect(() => {
-      console.log("Endring i liste, burde oppdater graf")
-      setDegreesPerColor(360/colorList.length);
-      setPieChartValues(getPieChartColors);
+      setPieChartValues(getPieChartColors());
     }, [colorList]);
 
     /*Returnerer en string som brukes til å sette fargene i pie chart
@@ -15,6 +13,7 @@ const MockupGraph = ({ colorList }) => {
     * hvor gradene er start- og slutt-sted for hver enkelt farge
     */
   const getPieChartColors = () => {
+    const degreesPerColor = (360/colorList.length);
     let string = "conic-gradient("
     colorList.map((value, i) => (i+1 === colorList.length ? string = string + colorList[i] + " " + degreesPerColor*i + "deg " + (degreesPerColor*i+degreesPerColor) + "deg" : string = string + colorList[i] + " " + degreesPerColor*i + "deg " + (degreesPerColor*i+degreesPerColor) + "deg, "))
     string = string + ")"
@@ -24,14 +23,10 @@ const MockupGraph = ({ colorList }) => {
 
   return (
     <div className="mockup-graph">
-        {/*<div className="mockup-color-bar">
-            <MockupColorPicker  topColorPicker={true} chosenColor={colorMain} setChosenColor={setColorMain} colorList={colorList}></MockupColorPicker>
-            <MockupColorPicker  bottomColorPicker={true} chosenColor={colorSecondary} setChosenColor={setColorSecondary} colorList={colorList}></MockupColorPicker>
-  </div>*/}
         <div>
             <div class="piechart" style={{backgroundImage: pieChartValues}}></div>
         </div>
-        <p>Kakediagrammet viser <br></br>alle fargene dine samlet <br></br>:) <br></br><br></br> Psst! Flere charts kommer</p>
+        <p>Kakediagrammet viser <br></br>alle fargene dine samlet! <br></br><br></br></p>
     </div>
   );
 };
