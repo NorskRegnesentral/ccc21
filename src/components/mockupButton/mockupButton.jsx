bed25c2e9fe11aa9fee77d285bb4d3438ef8f046
Kamilla Mortensen <kamilla@nr.no>
2022-08-04 16:04:51 +0200

    logic for mockup button

--- src/components/mockupButton/mockupButton.jsx
+++ src/components/mockupButton/mockupButton.jsx
@@ -2,13 +2,32 @@ import "./mockupButton.css";
 import React, {useState, useEffect} from "react";
 import MockupColorPicker from "../mockupColorPicker/mockupColorPicker";
 import { useTranslation } from 'react-i18next';
-import { checkColors } from "../../color-checker";
+import { color } from "@mui/system";
 
-const MockupButton = ({ colorList, color1, color2 }) => {
+const MockupButton = ({ colorList }) => {
   const { t } = useTranslation();
-  const [colorMain, setColorMain] = useState(color1);
-  const [colorSecondary, setColorSecondary] = useState(color2);
+  const [colorMain, setColorMain] = useState(colorList.length >= 2 ? colorList[0] : "#ffffff"); 
+  const [colorSecondary, setColorSecondary] = useState(colorList.length >= 2 ? colorList[1] : "#000000");
+
+  useEffect(() => {
+    if(colorList.length >= 2){
+      if(colorList.length == 2) setColorMain(colorList[0]); setColorSecondary(colorList[1]);
+      /*if(!colorList.includes(colorMain)) colorList[0] == colorSecondary ? setColorMain(colorList[1]) : setColorMain(colorList[0]);
+      if(!colorList.includes(colorSecondary)) colorList[0] == colorMain ? setColorSecondary(colorList[1]) : setColorSecondary(colorList[0]);*/
+    }
+    else {
+      if(colorList.length == 1){
+        setColorMain(colorList[0]);
+        setColorSecondary(colorList[0]);
+        return;
+      }
+      setColorMain("#ffffff");
+      setColorSecondary("#ffffff");
+      return;
+    }
+  }, [colorList]);
  
+  console.log(colorList.length)
   return (
     <div className="mockupElement">
     <div className="mockup-color-bar">
@@ -20,7 +39,7 @@ const MockupButton = ({ colorList, color1, color2 }) => {
         {t('ok')}  
       </button>
       <button style={{ color: colorSecondary, backgroundColor: colorMain, border: "2px solid "}} className="mockupn" >
-        {t('avbryt')}  
+        {t('cancel')}  
       </button>
     </div>
 </div>
