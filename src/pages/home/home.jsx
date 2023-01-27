d498bdd95f0a349767a490e65aa981d4df129314
Thor Kristoffersen <thor@nr.no>
2023-01-27 12:57:20 +0100

    Put sections in tabs

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 import "./home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useEffect, useState } from "react";
 import ContrastTable from "../../components/contrastTable/contrastTable";
 import MockupTextBox from "../../components/mockupTextBox/mockupTextBox";
@@ -25,6 +26,8 @@ import MockupButton from "../../components/mockupButton/mockupButton";
 import ContrastSummaryBox from "../../components/contrastSummaryBox/contrastSummaryBox";
 import ContrastSummary from "../../components/contrastSummary/contrastSummary";
 import ColorPalette from "../../components/colorPalette/colorPalette";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 function Home() {
   const { t } = useTranslation();
@@ -84,82 +87,110 @@ function Home() {
         />
       </div>
       <div className="rightSideBar">
-        <div className="aboutSection">
-          <div className="aboutSectionLeft">
-            <div className="row">
-              <InvertColorsRoundedIcon></InvertColorsRoundedIcon>
-              <h1 className="big-title">{t('about-title')}</h1>
-            </div>
-            <div>
-            <p className="p-small-about">{t('about-description')} </p>
-            {/*<p className="p-small-about">{t('about-wcag')}</p>*/}
-            <a href="https://www.uutilsynet.no/fremtidig-regelverk/wcag-21-standarden/140"  target="_blank" rel="noreferrer noopener">
-              <button className="primaryButton">{t('about-button-linking-to-wcag')}</button>
-            </a> 
-            </div>
-          </div>
-          <div className="aboutSectionRight">
-            <ul className="aboutSectionTextBoxContainer" aria-label={t('about-wcag-list-heading')}>
-              <li>
-                <TextBox title={t('contrast-summary-aaa')} titleIcon={<TextFieldsIcon/>} 
-                backgroundColor={contrastColors.AAA} mainText={t('about-wcag-aaa')}></TextBox>
-              </li>
-              <li>
-                <TextBox title={t('contrast-summary-aa')} titleIcon={<TextFieldsIcon/>} 
-                backgroundColor={contrastColors.AA} mainText={t('about-wcag-aa')}></TextBox>
-              </li>
-              <li>
-                <TextBox title={t('contrast-summary-nontext-aa')} titleIcon={<AutoAwesomeMosaicIcon/>} 
-                backgroundColor={contrastColors.AANontext} mainText={t('about-wcag-nontext-aa')}></TextBox>
-              </li>
-              <li>
-                <TextBox title={t('contrast-summary-low')} titleIcon={<VisibilityOffIcon/>} 
-                backgroundColor={contrastColors.none} mainText={t('about-wcag-low')}></TextBox>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="colorPaletteSection">
-          <fieldset className="default-palettes-container">
-            <legend className="mockup-section-title">
-              <ColorizeIcon></ColorizeIcon>
-              <h1 className="big-title">{t('try-default-palettes')}</h1>
-            </legend>
-            {Object.values(defaultColorPalettes).map((palette, index) => (
-              <div className="default-palette-container" onClick={()=>setColorList(palette)}>
-                <input id={"color-palette-"+index} type="radio"  className="radio-button"  checked={colorList == palette ? true : false}/>   
-                <ColorPalette colors={palette} labelId={"color-palette-"+index}></ColorPalette>
+        <Tabs defaultActiveKey="about" id="main-tab-group" className="mb-3">
+          <Tab eventKey="about" title="Om">
+            <div className="aboutSection">
+              <div className="aboutSectionLeft">
+                <div className="row">
+                  <InvertColorsRoundedIcon></InvertColorsRoundedIcon>
+                  <h1 className="big-title">{t('about-title')}</h1>
+                </div>
+                <div>
+                  <p className="p-small-about">{t('about-description')} </p>
+                  {/*<p className="p-small-about">{t('about-wcag')}</p>*/}
+                  <a href="https://www.uutilsynet.no/fremtidig-regelverk/wcag-21-standarden/140"  target="_blank" rel="noreferrer noopener">
+                    <button className="primaryButton">{t('about-button-linking-to-wcag')}</button>
+                  </a> 
+                </div>
               </div>
-            ))}
-          </fieldset>
-          <div className="speech-bubble">
-            <h2 className="speech-bubble-text">
-            {t('try-palette-or-try-yourself')}
-            </h2>
-          </div>
-        </div>
-        <div className="tableSection">
-          <div className="see-contrast-heading">
-            <ColorLensIcon></ColorLensIcon>
-            <h1 className="big-title">{t('table-section-title')}</h1>
-          </div>
-          <ContrastTable aria-details={t('contrast-table-title')} contrastMatrix={contrastMatrix}></ContrastTable>
-        </div>
+              <div className="aboutSectionRight">
+                <ul className="aboutSectionTextBoxContainer" aria-label={t('about-wcag-list-heading')}>
+                  <li>
+                    <TextBox title={t('contrast-summary-aaa')} titleIcon={<TextFieldsIcon/>} 
+                             backgroundColor={contrastColors.AAA} mainText={t('about-wcag-aaa')}></TextBox>
+                  </li>
+                  <li>
+                    <TextBox title={t('contrast-summary-aa')} titleIcon={<TextFieldsIcon/>} 
+                             backgroundColor={contrastColors.AA} mainText={t('about-wcag-aa')}></TextBox>
+                  </li>
+                  <li>
+                    <TextBox title={t('contrast-summary-nontext-aa')} titleIcon={<AutoAwesomeMosaicIcon/>} 
+                             backgroundColor={contrastColors.AANontext} mainText={t('about-wcag-nontext-aa')}></TextBox>
+                  </li>
+                  <li>
+                    <TextBox title={t('contrast-summary-low')} titleIcon={<VisibilityOffIcon/>} 
+                             backgroundColor={contrastColors.none} mainText={t('about-wcag-low')}></TextBox>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Tab>
+          <Tab eventKey="palette" title="Fargepalett">
+            <div className="colorPaletteSection">
+              <fieldset className="default-palettes-container">
+                <legend className="mockup-section-title">
+                  <ColorizeIcon></ColorizeIcon>
+                  <h1 className="big-title">{t('try-default-palettes')}</h1>
+                </legend>
+                {Object.values(defaultColorPalettes).map((palette, index) => (
+                  <div className="default-palette-container" onClick={()=>setColorList(palette)}>
+                    <input id={"color-palette-"+index} type="radio"  className="radio-button"  checked={colorList == palette ? true : false}/>   
+                    <ColorPalette colors={palette} labelId={"color-palette-"+index}></ColorPalette>
+                  </div>
+                ))}
+              </fieldset>
+              <div className="speech-bubble">
+                <h2 className="speech-bubble-text">
+                  {t('try-palette-or-try-yourself')}
+                </h2>
+              </div>
+            </div>
+          </Tab>
+          <Tab eventKey="table" title="Tabell">
+            <div className="tableSection">
+              <div className="see-contrast-heading">
+                <ColorLensIcon></ColorLensIcon>
+                <h1 className="big-title">{t('table-section-title')}</h1>
+              </div>
+              <ContrastTable aria-details={t('contrast-table-title')} contrastMatrix={contrastMatrix}></ContrastTable>
+            </div>
+          </Tab>
+          <Tab eventKey="testing" title="Test">
+            <div className="mockupSection">
+              <div className="mockup-section-title">
+                <HandymanIcon></HandymanIcon>
+                <h1 className="big-title">{t('mockup-section-title')}</h1>
+              </div>
+              <div className="aboutSectionTextBoxContainer">
+                <MockupTextBox colorList={colorList} title={t('mockup-textbox-header')}  
+                               mainText={t('mockup-textbox-maintext')} 
+                               titleIcon={<AutoAwesomeIcon/>} color1={colorList[3]} color2={colorList[2]}></MockupTextBox>
+                <MockupIllustration colorList={colorList} color1={colorList[3]} color2={colorList[2]}></MockupIllustration>
+                <MockupGraph colorList={colorList}></MockupGraph>
+                <MockupButton colorList={colorList} contrastMatrix={contrastMatrix} color1={colorList[3]} color2={colorList[2]}></MockupButton>
+              </div>
+            </div>
+          </Tab>
+          <Tab eventKey="summary" title="Oppsummering">
+            <div className="summarySection">
+              <div className="see-contrast-heading">
+                <InventoryRoundedIcon/>
+                <h1 className="big-title">{t('recommendation-section-title')}</h1>
+              </div>
+              <div className="aboutSectionTextBoxContainer">
+                <ContrastSummaryBox title={t('contrast-summary-aaa')} titleIcon={<TextFieldsIcon/>} backgroundColor={contrastColors.AAA}
+                                    colorCombinationsList={getContrastList(contrastMatrix, w.aaaTextMin, w.contrastMax)}></ContrastSummaryBox>
+                <ContrastSummaryBox title={t('contrast-summary-aa')} titleIcon={<TextFieldsIcon/>} backgroundColor={contrastColors.AA}
+                                    colorCombinationsList={getContrastList(contrastMatrix, w.aaTextMin, w.aaTextMax)}></ContrastSummaryBox>
+                <ContrastSummaryBox title={t('contrast-summary-nontext-aa')} titleIcon={<AutoAwesomeMosaicIcon/>} backgroundColor={contrastColors.AANontext}
+                                    colorCombinationsList={getContrastList(contrastMatrix, w.aaNonTextMin, w.aaNonTextMax)}></ContrastSummaryBox>
+                <ContrastSummaryBox title={t('contrast-summary-low')} titleIcon={<VisibilityOffIcon/>} backgroundColor={contrastColors.none}
+                                    colorCombinationsList={getContrastList(contrastMatrix, w.contrastMin, w.aaNonTextMin)}></ContrastSummaryBox>
+              </div>
+            </div>
+          </Tab>
+        </Tabs>
         {/*<div><ContrastSummary contrastMatrix={contrastMatrix}></ContrastSummary></div>*/}
-        <div className="mockupSection">
-          <div className="mockup-section-title">
-            <HandymanIcon></HandymanIcon>
-            <h1 className="big-title">{t('mockup-section-title')}</h1>
-          </div>
-          <div className="aboutSectionTextBoxContainer">
-            <MockupTextBox colorList={colorList} title={t('mockup-textbox-header')}  
-            mainText={t('mockup-textbox-maintext')} 
-            titleIcon={<AutoAwesomeIcon/>} color1={colorList[3]} color2={colorList[2]}></MockupTextBox>
-            <MockupIllustration colorList={colorList} color1={colorList[3]} color2={colorList[2]}></MockupIllustration>
-            <MockupGraph colorList={colorList}></MockupGraph>
-            <MockupButton colorList={colorList} contrastMatrix={contrastMatrix} color1={colorList[3]} color2={colorList[2]}></MockupButton>
-          </div>
-        </div>
         {/* 
          <div>
           <div className="see-contrast-heading">
@@ -170,22 +201,6 @@ function Home() {
           <ContrastSummary contrastMatrix={contrastMatrix}></ContrastSummary>
         </div>
         */}
-        <div className="summarySection">
-          <div className="see-contrast-heading">
-            <InventoryRoundedIcon/>
-            <h1 className="big-title">{t('recommendation-section-title')}</h1>
-          </div>
-          <div className="aboutSectionTextBoxContainer">
-            <ContrastSummaryBox title={t('contrast-summary-aaa')} titleIcon={<TextFieldsIcon/>} backgroundColor={contrastColors.AAA}
-              colorCombinationsList={getContrastList(contrastMatrix, w.aaaTextMin, w.contrastMax)}></ContrastSummaryBox>
-            <ContrastSummaryBox title={t('contrast-summary-aa')} titleIcon={<TextFieldsIcon/>} backgroundColor={contrastColors.AA}
-              colorCombinationsList={getContrastList(contrastMatrix, w.aaTextMin, w.aaTextMax)}></ContrastSummaryBox>
-            <ContrastSummaryBox title={t('contrast-summary-nontext-aa')} titleIcon={<AutoAwesomeMosaicIcon/>} backgroundColor={contrastColors.AANontext}
-              colorCombinationsList={getContrastList(contrastMatrix, w.aaNonTextMin, w.aaNonTextMax)}></ContrastSummaryBox>
-            <ContrastSummaryBox title={t('contrast-summary-low')} titleIcon={<VisibilityOffIcon/>} backgroundColor={contrastColors.none}
-              colorCombinationsList={getContrastList(contrastMatrix, w.contrastMin, w.aaNonTextMin)}></ContrastSummaryBox>
-          </div>
-        </div>
         <Footer
         backgroundColor="#fcfcfc"
         textColor="#1f1235"  
